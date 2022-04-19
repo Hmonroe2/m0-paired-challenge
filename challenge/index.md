d41ddc048f273642a3afb867e9063059150118e2
Hunter Monroe <huntis@outlook.com>
2022-04-18 20:26:17 -0600

    initial commit

---
+++ challenge/index.md
@@ -0,0 +1,8 @@
+# 5 book titles
+  * JavaScript and jquary
+  * Harry Potter Chamber of sercrets
+  * How to change your mindset.
+  * The little book of talent
+  * The little red book of selling.
+
+  ```JAVASCRIPT
---
+++ challenge/indexJava.js
@@ -0,0 +1,10 @@
+var haveRead = true;
+var booksRead= 4;
+var bookType = ("self help", "fiction", "nonfiction");
+var maleAuthor= true;
+var hardCover= true;
+var receivedMethod= ("gift", "school", "freeBook")
+
+
+var harryPotter series = ("book1", "book2", "book3", "book4", "book5", "book6");
+// This variable declares book numbers 
